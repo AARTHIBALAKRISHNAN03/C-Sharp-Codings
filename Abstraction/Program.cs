7adc976e3ebe50b13fcff79bd0bb51e8bc217b3c
AARTHIBALAKRISHNAN03 <149576377+AARTHIBALAKRISHNAN03@users.noreply.github.com>
2024-05-09 13:52:50 +0530

    Add files via upload

---
+++ Abstraction/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+
+public abstract class Shape
+{
+    
+    public abstract double CalculateArea();
+}
+
+
+public class Circle : Shape
+{
+    private double radius;
+
+  
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    
+    public override double CalculateArea()
+    {
+        return Math.PI * radius * radius;
+    }
+}
+
+
+public class Rectangle : Shape
+{
+    private double width;
+    private double height;
+
+  
+    public Rectangle(double width, double height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
+    
+    public override double CalculateArea()
+    {
+        return width * height;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 6);
+
+        
+        
+        Console.WriteLine("Area of Circle: " + circle.CalculateArea());
+        Console.WriteLine("Area of Rectangle: " + rectangle.CalculateArea());
+    }
+}
