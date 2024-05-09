dc5931093d1f7cf2451bef9507cd0f2575cd40cf
AARTHIBALAKRISHNAN03 <149576377+AARTHIBALAKRISHNAN03@users.noreply.github.com>
2024-05-09 13:56:22 +0530

    Add files via upload

---
+++ PolymorphismOverloading/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class MathOperations
+{
+    
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Add(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+
+  
+    public double Add(double a, double b)
+    {
+        return a + b;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MathOperations math = new MathOperations();
+
+        
+        int result1 = math.Add(5, 3);
+        int result2 = math.Add(10, 20, 30);
+        double result3 = math.Add(2.5, 3.7);
+
+        Console.WriteLine("Result1: " + result1); 
+        Console.WriteLine("Result2: " + result2); 
+        Console.WriteLine("Result3: " + result3); 
+    }
+}
