7b6f48c4f6de4fbf4b9187f19be265bb7cb01b8c
AARTHIBALAKRISHNAN03 <149576377+AARTHIBALAKRISHNAN03@users.noreply.github.com>
2024-05-09 13:55:35 +0530

    Add files via upload

---
+++ Exception/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Enter the first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            
+            int result = num1 / num2;
+
+            Console.WriteLine("Average: " + result);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Please enter valid integer numbers.");
+        }
+        
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Error: Cannot divide by zero. Please enter a non-zero number as the second number.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Execution completed.");
+        }
+    }
+}
