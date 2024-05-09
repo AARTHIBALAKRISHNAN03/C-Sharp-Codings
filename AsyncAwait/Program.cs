746b59fe55030b9e49a08a333e47db27136b0af2
AARTHIBALAKRISHNAN03 <149576377+AARTHIBALAKRISHNAN03@users.noreply.github.com>
2024-05-09 13:53:31 +0530

    Add files via upload

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Start downloading data...");
+
+        
+        string data = await DownloadDataAsync();
+
+       
+        Console.WriteLine($"Downloaded data: {data}");
+
+        Console.WriteLine("Data processing complete.");
+    }
+
+    static async Task<string> DownloadDataAsync()
+    {
+        
+        await Task.Delay(3000); 
+
+        
+        return "This is the downloaded data.";
+    }
+}
